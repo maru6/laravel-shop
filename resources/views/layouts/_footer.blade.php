81e6060008c0fd78d89d2e91d35180b0c13b9436
vagrant <vagrant@homestead>
2018-08-12 12:12:03 +0000

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+	<div class="container">
+		<p class="pull-left">
+			由<a href="#" target="_bland">maru</a>设计和编码 <span style="color:#e27575;font-size: 14px;">❤</span>
+		</p>
+
+		<p class="pull-right"><a href="dnakv32@gmail.com">联系我</a></p>
+	</div>
+</footer>
